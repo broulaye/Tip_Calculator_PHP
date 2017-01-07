e8669850359de565ace6f29644976b962e219b30
System Administrator <root@Broulayes-MBP.fios-router.home>
2017-01-06 23:03:47 -0500

    added radio button

--- calc.php
+++ calc.php
@@ -8,39 +8,29 @@
 	<body>
 		<?php
 			$Amount = $_POST['Amount'];
-			$dropdown = $_POST['dropdown'];
+			$dropdown = $_POST['calc'];
 		?>
 		<form action= "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
-			<select name = "dropdown">
-				<option></option>
-				<option value = "5%">five percent</option>
-				<option value = "10%">ten percent</option>
-				<option value = "15%">fithteen percent</option>
-				<option value = "20%">twenty percent</option>
-			</select><br></br>
+			<input type = "radio" name = "calc" value = "ten"> 10%
+            <input type = "radio" name = "calc" value = "fifteen"> 15%
+            <input type = "radio" name = "calc" value = "twenty"> 20% <br></br>
 			Enter Amount: <input type="text" name="Amount" required/><br></br>
 			<input type="submit" name="Submit"/>
 			<?php
 			switch ($dropdown) {
-				case 'five percent':
-					$tip = 0.05 * $Amount;
-					$Amount = $Amount + $tip;
-					echo('<p>Tip: ' . '$' . $tip . '</p>');
-					echo('Bill Total: ' . '$' . $Amount);
-					break;
-				case 'ten percent':
+				case 'ten':
 					$tip = 0.10 * $Amount;
 					$Amount = $Amount + $tip;
 					echo('<p>Tip: ' . '$' . $tip . '</p>');
 					echo('Bill Total: ' . '$' . $Amount);
 					break;
-				case 'fifften percent':
+				case 'fifteen':
 					$tip = 0.15 * $Amount;
 					$Amount = $Amount + $tip;
 					echo('<p>Tip: ' . '$' . $tip . '</p>');
 					echo('Bill Total: ' . '$' . $Amount);
 					break;
-				case 'twenty percent':
+				case 'twenty':
 					$tip = 0.20 * $Amount;
 					$Amount = $Amount + $tip;
 					echo('<p>Tip: ' . '$' . $tip . '</p>');
